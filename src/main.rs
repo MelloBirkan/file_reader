95abeb5d8ae07da971f5611eec7a972869b12a8f
Mello Birkan <mgbirkan@icloud.com>
2023-12-30 03:10:01 -0300

    Implement file reading in main.rs
    Code that reads a file. The file's name is entered by the user, and error handling is provided for cases where the file doesn't exist or cannot be opened. The file's contents are then read line by line and printed.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Vec<String> = env::args().collect();
+    // Remove the first argument, which is the path to the executable
+    if let Some(string) = args.get_mut(0) {
+        *string = string.replace("file_reader", "");
+    }
+    let mut input = String::new();
+    println!("Enter a file name in this directory: {}", args[0]);
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Error reading line");
+    let path = format!("{}/{}", args[0],input.trim());
+
+    println!("Path: {}", &path);
+    let file = File::open(&path);
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            std::io::ErrorKind::NotFound => {
+                panic!("File not found: {}", error)
+            }
+            _ => {
+                panic!("Error opening file: {}", error)
+            }
+        },
+    };
+
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        match line {
+            Ok(line) => println!("{}", line),
+            Err(error) => {
+                panic!("Error reading line: {}", error)
+            }
+        }
+    }
 }
